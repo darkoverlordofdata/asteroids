4be92ce362f07eb0ddef98be6245ce1bf3d3ce23
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-08-28 21:14:30 -0700

    restore missing web/frameworks

---
+++ web/frameworks/runtime-src/proj.android-studio/app/src/com/darkoverlordofdata/alienzone/DisplayMessageActivity.java
@@ -0,0 +1,54 @@
+package com.darkoverlordofdata.alienzone;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.cocos2dx.javascript.AppActivity;
+
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String title = intent.getStringExtra(AppActivity.EXTRA_TITLE);
+        int score = intent.getIntExtra(AppActivity.EXTRA_SCORE, -1);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(title+" score ="+score);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
